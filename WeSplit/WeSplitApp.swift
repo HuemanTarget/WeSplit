4c77288bedf0bc586ce0c2f98b57f92d75f13e8c
HuemanTarget <joshbasche@live.com>
2021-02-22 18:23:34 -0800

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Joshua Basche on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
